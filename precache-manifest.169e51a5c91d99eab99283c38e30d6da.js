87a7e62dc35b65b0abbdbd187c76da0c05859225
unknown <alitahashakir@gmail.com>
2020-07-07 22:02:00 +0500

    Updates

---
+++ precache-manifest.169e51a5c91d99eab99283c38e30d6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b90a398c591462b992da6d3025adc87",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "350085bd0353717f47b4",
+    "url": "/monsters-rolodex/static/css/main.16bedc4d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350085bd0353717f47b4",
+    "url": "/monsters-rolodex/static/js/main.cb94d7e3.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
